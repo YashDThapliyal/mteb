e26bc08093541d3fd84fd73c9b45c30cdb42ccc1
YashDThapliyal <yashdt4321@gmail.com>
2025-01-11 22:01:13 -0800

    Update extract_unibench_datasets.py
    remove the wds_ prefix from datasets

--- mteb/tasks/Image/UniBenchIntegration/extract_unibench_datasets.py
+++ mteb/tasks/Image/UniBenchIntegration/extract_unibench_datasets.py
@@ -5,7 +5,9 @@ def extract_datasets(file_path):
         content = file.read()
     
     dataset_urls = re.findall(r'dataset_url="([^"]+)"', content)
-    unique_datasets = sorted(set(url.split("/")[-1] for url in dataset_urls))
+    
+    # Remove '/wds_' prefix and get unique dataset names
+    unique_datasets = sorted(set(url.split("/")[-1].replace("wds_", "") for url in dataset_urls))
     
     return unique_datasets
 
@@ -14,10 +16,8 @@ def save_to_file(datasets, output_file):
         for dataset in datasets:
             file.write(f"{dataset}\n")
 
-
 file_path = "/Users/yash/Documents/Stanford/mteb/mteb/tasks/Image/UniBenchIntegration/UniBench-Benchmarks.py"
 output_file = "/Users/yash/Documents/Stanford/mteb/mteb/tasks/Image/UniBenchIntegration/UniBenchDatasets.txt"
-
 
 datasets = extract_datasets(file_path)
 save_to_file(datasets, output_file)
